583854fa67084a923114f586955098a2471aca79
Skye M <skyemc608@gmail.com>
2022-01-23 21:25:07 -0600

    changed styling

---
+++ README.md
@@ -0,0 +1,1 @@
+Ok everyone, here's my xylophone. In my opinion, everyone needs atleast one!! So thank goodness for this amazing npm package called "xylophone react." What an absolute gift! If you enjoy my virtual xylophone, visit this link: https://www.npmjs.com/package/xylophone-react to learn about the package and how you can make your very own virtual xylophone!!
--- virtual-xylophone/src/Components/Xylo.js
+++ virtual-xylophone/src/Components/Xylo.js
@@ -7,27 +7,20 @@ class Xylo extends React.Component {
         super(props);
         this.state = { pressedKey: 0, currentOctave: -3};
         this.setPressedKey = this.setPressedKey.bind(this);
-        this.setOctave = this.setOctave.bind(this);
+            
       }
 
       setPressedKey(key) {
         this.setState({pressedKey: key})
 
     }
-    setOctave(e) {
-      
-        this.setState({currentOctave: e.target.value});
-        
-    }
+    
     
 render() {
 return(
 
 <>
-<h3>Enter A Value To Change The Octave</h3>
-    <input type="text" onChange={ this.setOctave} value={ this.state.currentOctave }/>
 
-    <button type="submit">Submit</button>
 
 <div id="xylo">
     <Xylophone pressedKey={this.setPressedKey} numberOfKeys={13} height={200} width={60} startingOctave={this.state.currentOctave} colors={[
